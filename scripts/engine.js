daa1459a0354ef00514af727bf6d68e0592a7352
allesonsales <107868418+allesonsales@users.noreply.github.com>
2024-11-19 13:08:49 -0300

    Update engine.js

--- scripts/engine.js
+++ scripts/engine.js
@@ -17,7 +17,7 @@ const state = {
         curretTime: 10,
     },
     actions: {
-        timerId: setInterval(randomsquare, 200),
+        timerId: setInterval(randomsquare, 50),
         countDownTimerid: setInterval(countDown, 1000),
     }
 }
